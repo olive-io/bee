17ca8b2f369fe8562edfef2d12ea78d18b16ea4a
lack <598223084@qq.com>
2024-01-09 11:28:30 +0800

    rename `parser`

--- parser/dataloader.go
+++ parser/dataloader.go
@@ -14,165 +14,140 @@
 
 package parser
 
-import (
-	"bufio"
-	"bytes"
-	"io"
-	"os"
-	"sort"
-	"strings"
-)
+// DataLoader-related helper methods
 
-// DataLoader contains parsed inventory representation
-// Note: Groups and Hosts fields contain all the groups and hosts, not only top-level
-type DataLoader struct {
-	Groups map[string]*Group
-	Hosts  map[string]*Host
-}
-
-// Group represents ansible group
-type Group struct {
-	Name     string
-	Vars     map[string]string
-	Hosts    map[string]*Host
-	Children map[string]*Group
-	Parents  map[string]*Group
-
-	DirectParents map[string]*Group
-	// Vars set in inventory
-	InventoryVars map[string]string
-	// Vars set in group_vars
-	FileVars map[string]string
-	// Projection of all parent inventory variables
-	AllInventoryVars map[string]string
-	// Projection of all parent group_vars variables
-	AllFileVars map[string]string
-}
-
-// Host represents ansible host
-type Host struct {
-	Name   string
-	Port   int
-	Vars   map[string]string
-	Groups map[string]*Group
-
-	DirectGroups map[string]*Group
-	// Vars set in inventory
-	InventoryVars map[string]string
-	// Vars set in host_vars
-	FileVars map[string]string
-}
-
-func NewDataLoader() *DataLoader {
-	dl := &DataLoader{}
-	dl.initData()
-	return dl
-}
-
-// ParseFile parses DataLoader represented as a file
-func (dl *DataLoader) ParseFile(f string) error {
-	bs, err := os.ReadFile(f)
-	if err != nil {
-		return err
-	}
-
-	return dl.Parse(bytes.NewReader(bs))
-}
-
-// ParseString parses DataLoader represented as a string
-func (dl *DataLoader) ParseString(input string) error {
-	return dl.Parse(strings.NewReader(input))
-}
-
-// Parse using some Reader
-func (dl *DataLoader) Parse(r io.Reader) error {
-	input := bufio.NewReader(r)
-	err := dl.parse(input)
-	if err != nil {
-		return err
-	}
-	dl.Reconcile()
-	return nil
-}
-
-// GroupMapListValues transforms map of Groups into Group list in lexical order
-func GroupMapListValues(mymap map[string]*Group) []*Group {
-	values := make([]*Group, len(mymap))
-
-	i := 0
-	for _, v := range mymap {
-		values[i] = v
-		i++
-	}
-	sort.Slice(values, func(i, j int) bool {
-		return values[i].Name < values[j].Name
-	})
-	return values
-}
-
-// HostMapListValues transforms map of Hosts into Host list in lexical order
-func HostMapListValues(mymap map[string]*Host) []*Host {
-	values := make([]*Host, len(mymap))
-
-	i := 0
-	for _, v := range mymap {
-		values[i] = v
-		i++
-	}
-	sort.Slice(values, func(i, j int) bool {
-		return values[i].Name < values[j].Name
-	})
-	return values
-}
-
-// HostsToLower transforms all host names to lowercase
-func (dl *DataLoader) HostsToLower() {
-	dl.Hosts = hostMapToLower(dl.Hosts, false)
-	for _, group := range dl.Groups {
-		group.Hosts = hostMapToLower(group.Hosts, true)
-	}
-}
-
-func hostMapToLower(hosts map[string]*Host, keysOnly bool) map[string]*Host {
-	newHosts := make(map[string]*Host, len(hosts))
-	for hostname, host := range hosts {
-		hostname = strings.ToLower(hostname)
-		if !keysOnly {
-			host.Name = hostname
-		}
-		newHosts[hostname] = host
-	}
-	return newHosts
-}
-
-// GroupsToLower transforms all group names to lowercase
-func (dl *DataLoader) GroupsToLower() {
-	dl.Groups = groupMapToLower(dl.Groups, false)
+// Reconcile ensures inventory basic rules, run after updates.
+// After initial inventory file processing, only direct relationships are set.
+//
+// This method:
+//   - (re)sets Children and Parents for hosts and groups
+//   - ensures that mandatory groups exist
+//   - calculates variables for hosts and groups
+func (dl *DataLoader) Reconcile() {
+	// Clear all computed data
 	for _, host := range dl.Hosts {
-		host.DirectGroups = groupMapToLower(host.DirectGroups, true)
-		host.Groups = groupMapToLower(host.Groups, true)
+		host.clearData()
 	}
-}
+	// a group can be empty (with no hosts in it), so the previous method will not clean it
+	// on the other hand, a group could have been attached to a host by a user, but not added to the inventory.Groups map
+	// so it's safer just to clean everything
+	for _, group := range dl.Groups {
+		group.clearData(make(map[string]struct{}, len(dl.Groups)))
+	}
 
-func (group *Group) String() string {
-	return group.Name
-}
+	allGroup := dl.getOrCreateGroup("all")
+	ungroupedGroup := dl.getOrCreateGroup("ungrouped")
+	ungroupedGroup.DirectParents[allGroup.Name] = allGroup
 
-func (host *Host) String() string {
-	return host.Name
-}
-
-func groupMapToLower(groups map[string]*Group, keysOnly bool) map[string]*Group {
-	newGroups := make(map[string]*Group, len(groups))
-	for groupname, group := range groups {
-		groupname = strings.ToLower(groupname)
-		if !keysOnly {
-			group.Name = groupname
-			group.DirectParents = groupMapToLower(group.DirectParents, true)
-			group.Parents = groupMapToLower(group.Parents, true)
-			group.Children = groupMapToLower(group.Children, true)
+	// First, ensure that inventory.Groups contains all the groups
+	for _, host := range dl.Hosts {
+		for _, group := range host.DirectGroups {
+			dl.Groups[group.Name] = group
+			for _, ancestor := range group.ListParentGroupsOrdered() {
+				dl.Groups[ancestor.Name] = ancestor
+			}
 		}
-		newGroups[groupname] = group
 	}
-	return newGroups
+
+	// Calculate intergroup relationships
+	for _, group := range dl.Groups {
+		group.DirectParents[allGroup.Name] = allGroup
+		for _, ancestor := range group.ListParentGroupsOrdered() {
+			group.Parents[ancestor.Name] = ancestor
+			ancestor.Children[group.Name] = group
+		}
+	}
+
+	// Now set hosts for groups and groups for hosts
+	for _, host := range dl.Hosts {
+		host.Groups[allGroup.Name] = allGroup
+		for _, group := range host.DirectGroups {
+			group.Hosts[host.Name] = host
+			host.Groups[group.Name] = group
+			for _, parent := range group.Parents {
+				group.Parents[parent.Name] = parent
+				parent.Children[group.Name] = group
+				parent.Hosts[host.Name] = host
+				host.Groups[parent.Name] = parent
+			}
+		}
+	}
+	dl.reconcileVars()
+}
+
+func (host *Host) clearData() {
+	host.Groups = make(map[string]*Group)
+	host.Vars = make(map[string]string)
+	for _, group := range host.DirectGroups {
+		group.clearData(make(map[string]struct{}, len(host.Groups)))
+	}
+}
+
+func (group *Group) clearData(visited map[string]struct{}) {
+	if _, ok := visited[group.Name]; ok {
+		return
+	}
+	group.Hosts = make(map[string]*Host)
+	group.Parents = make(map[string]*Group)
+	group.Children = make(map[string]*Group)
+	group.Vars = make(map[string]string)
+	group.AllInventoryVars = nil
+	group.AllFileVars = nil
+	visited[group.Name] = struct{}{}
+	for _, parent := range group.DirectParents {
+		parent.clearData(visited)
+	}
+}
+
+// getOrCreateGroup return group from inventory if exists or creates empty Group with given name
+func (dl *DataLoader) getOrCreateGroup(groupName string) *Group {
+	if group, ok := dl.Groups[groupName]; ok {
+		return group
+	}
+	g := &Group{
+		Name:     groupName,
+		Hosts:    make(map[string]*Host),
+		Vars:     make(map[string]string),
+		Children: make(map[string]*Group),
+		Parents:  make(map[string]*Group),
+
+		DirectParents: make(map[string]*Group),
+		InventoryVars: make(map[string]string),
+		FileVars:      make(map[string]string),
+	}
+	dl.Groups[groupName] = g
+	return g
+}
+
+// getOrCreateHost return host from inventory if exists or creates empty Host with given name
+func (dl *DataLoader) getOrCreateHost(hostName string) *Host {
+	if host, ok := dl.Hosts[hostName]; ok {
+		return host
+	}
+	h := &Host{
+		Name:   hostName,
+		Port:   22,
+		Groups: make(map[string]*Group),
+		Vars:   make(map[string]string),
+
+		DirectGroups:  make(map[string]*Group),
+		InventoryVars: make(map[string]string),
+		FileVars:      make(map[string]string),
+	}
+	dl.Hosts[hostName] = h
+	return h
+}
+
+// addValues fills `to` map with values from `from` map
+func addValues(to map[string]string, from map[string]string) {
+	for k, v := range from {
+		to[k] = v
+	}
+}
+
+// copyStringMap creates a non-deep copy of the map
+func copyStringMap(from map[string]string) map[string]string {
+	result := make(map[string]string, len(from))
+	addValues(result, from)
+	return result
 }
--- parser/parser.go
+++ parser/parser.go
@@ -16,285 +16,163 @@ package parser
 
 import (
 	"bufio"
-	"fmt"
-	"math"
-	"regexp"
-	"strconv"
+	"bytes"
+	"io"
+	"os"
+	"sort"
 	"strings"
-
-	"github.com/google/shlex"
 )
 
-// state enum
-type state int
-
-const (
-	hostsState    state = 0
-	childrenState state = 1
-	varsState     state = 2
-)
-
-func getState(str string) (state, bool) {
-	var result state
-	var ok bool = true
-	if str == "" || str == "hosts" {
-		result = hostsState
-	} else if str == "children" {
-		result = childrenState
-	} else if str == "vars" {
-		result = varsState
-	} else {
-		ok = false
-	}
-	return result, ok
+// DataLoader contains parsed inventory representation
+// Note: Groups and Hosts fields contain all the groups and hosts, not only top-level
+type DataLoader struct {
+	Groups map[string]*Group
+	Hosts  map[string]*Host
 }
 
-// state enum end
+// Group represents ansible group
+type Group struct {
+	Name     string
+	Vars     map[string]string
+	Hosts    map[string]*Host
+	Children map[string]*Group
+	Parents  map[string]*Group
 
-func (dl *DataLoader) initData() {
-	dl.Groups = make(map[string]*Group)
-	dl.Hosts = make(map[string]*Host)
+	DirectParents map[string]*Group
+	// Vars set in inventory
+	InventoryVars map[string]string
+	// Vars set in group_vars
+	FileVars map[string]string
+	// Projection of all parent inventory variables
+	AllInventoryVars map[string]string
+	// Projection of all parent group_vars variables
+	AllFileVars map[string]string
 }
 
-// parser performs parsing of inventory file from some Reader
-func (dl *DataLoader) parse(reader *bufio.Reader) error {
-	// This regexp is copy-pasted from ansible sources
-	sectionRegex := regexp.MustCompile(`^\[([^:\]\s]+)(?::(\w+))?\]\s*(?:\#.*)?$`)
-	scanner := bufio.NewScanner(reader)
-	activeState := hostsState
-	activeGroup := dl.getOrCreateGroup("ungrouped")
+// Host represents ansible host
+type Host struct {
+	Name   string
+	Port   int
+	Vars   map[string]string
+	Groups map[string]*Group
 
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if strings.HasPrefix(line, ";") || strings.HasPrefix(line, "#") || line == "" {
-			continue
-		}
-		matches := sectionRegex.FindAllStringSubmatch(line, -1)
-		if matches != nil {
-			activeGroup = dl.getOrCreateGroup(matches[0][1])
-			var ok bool
-			if activeState, ok = getState(matches[0][2]); !ok {
-				return fmt.Errorf("section [%s] has unknown type: %s", line, matches[0][2])
-			}
+	DirectGroups map[string]*Group
+	// Vars set in inventory
+	InventoryVars map[string]string
+	// Vars set in host_vars
+	FileVars map[string]string
+}
 
-			continue
-		} else if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
-			return fmt.Errorf("invalid section entry: '%s'. Make sure that there are no spaces or other characters in the section entry", line)
-		}
+func NewDataLoader() *DataLoader {
+	dl := &DataLoader{}
+	dl.initData()
+	return dl
+}
 
-		if activeState == hostsState {
-			hosts, err := dl.getHosts(line, activeGroup)
-			if err != nil {
-				return err
-			}
-			for _, host := range hosts {
-				host.DirectGroups[activeGroup.Name] = activeGroup
-				dl.Hosts[host.Name] = host
-				if activeGroup.Name != "ungrouped" {
-					delete(host.DirectGroups, "ungrouped")
-				}
-			}
-		}
-		if activeState == childrenState {
-			parsed, err := shlex.Split(line)
-			if err != nil {
-				return err
-			}
-			groupName := parsed[0]
-			newGroup := dl.getOrCreateGroup(groupName)
-			newGroup.DirectParents[activeGroup.Name] = activeGroup
-			dl.Groups[line] = newGroup
-		}
-		if activeState == varsState {
-			k, v, err := splitKV(line)
-			if err != nil {
-				return err
-			}
-			activeGroup.InventoryVars[k] = v
-		}
+// ParseFile parses DataLoader represented as a file
+func (dl *DataLoader) ParseFile(f string) error {
+	bs, err := os.ReadFile(f)
+	if err != nil {
+		return err
 	}
-	dl.Groups[activeGroup.Name] = activeGroup
+
+	return dl.Parse(bytes.NewReader(bs))
+}
+
+// ParseString parses DataLoader represented as a string
+func (dl *DataLoader) ParseString(input string) error {
+	return dl.Parse(strings.NewReader(input))
+}
+
+// Parse using some Reader
+func (dl *DataLoader) Parse(r io.Reader) error {
+	input := bufio.NewReader(r)
+	err := dl.parse(input)
+	if err != nil {
+		return err
+	}
+	dl.Reconcile()
 	return nil
 }
 
-// getHosts parses given "host" line from inventory
-func (dl *DataLoader) getHosts(line string, group *Group) (map[string]*Host, error) {
-	parts, err := shlex.Split(line)
-	if err != nil {
-		return nil, err
+// GroupMapListValues transforms map of Groups into Group list in lexical order
+func GroupMapListValues(mymap map[string]*Group) []*Group {
+	values := make([]*Group, len(mymap))
+
+	i := 0
+	for _, v := range mymap {
+		values[i] = v
+		i++
 	}
-	hostpattern, port, err := getHostPort(parts[0])
-	if err != nil {
-		return nil, err
+	sort.Slice(values, func(i, j int) bool {
+		return values[i].Name < values[j].Name
+	})
+	return values
+}
+
+// HostMapListValues transforms map of Hosts into Host list in lexical order
+func HostMapListValues(mymap map[string]*Host) []*Host {
+	values := make([]*Host, len(mymap))
+
+	i := 0
+	for _, v := range mymap {
+		values[i] = v
+		i++
 	}
-	hostnames, err := expandHostPattern(hostpattern)
-	if err != nil {
-		return nil, err
+	sort.Slice(values, func(i, j int) bool {
+		return values[i].Name < values[j].Name
+	})
+	return values
+}
+
+// HostsToLower transforms all host names to lowercase
+func (dl *DataLoader) HostsToLower() {
+	dl.Hosts = hostMapToLower(dl.Hosts, false)
+	for _, group := range dl.Groups {
+		group.Hosts = hostMapToLower(group.Hosts, true)
 	}
-	result := make(map[string]*Host, len(hostnames))
-	for _, hostname := range hostnames {
-		params := parts[1:]
-		vars := make(map[string]string, len(params))
-		for _, param := range params {
-			k, v, err := splitKV(param)
-			if err != nil {
-				return nil, err
-			}
-			vars[k] = v
+}
+
+func hostMapToLower(hosts map[string]*Host, keysOnly bool) map[string]*Host {
+	newHosts := make(map[string]*Host, len(hosts))
+	for hostname, host := range hosts {
+		hostname = strings.ToLower(hostname)
+		if !keysOnly {
+			host.Name = hostname
 		}
-
-		host := dl.getOrCreateHost(hostname)
-		host.Port = port
-		host.DirectGroups[group.Name] = group
-		addValues(host.InventoryVars, vars)
-
-		result[host.Name] = host
+		newHosts[hostname] = host
 	}
-	return result, nil
+	return newHosts
 }
 
-// splitKV splits `key=value` into two string: key and value
-func splitKV(kv string) (string, string, error) {
-	keyval := strings.SplitN(kv, "=", 2)
-	if len(keyval) == 1 {
-		return "", "", fmt.Errorf("bad key=value pair supplied: %s", kv)
+// GroupsToLower transforms all group names to lowercase
+func (dl *DataLoader) GroupsToLower() {
+	dl.Groups = groupMapToLower(dl.Groups, false)
+	for _, host := range dl.Hosts {
+		host.DirectGroups = groupMapToLower(host.DirectGroups, true)
+		host.Groups = groupMapToLower(host.Groups, true)
 	}
-	return strings.TrimSpace(keyval[0]), strings.TrimSpace(keyval[1]), nil
 }
 
-// getHostPort splits string like `host-[a:b]-c:22` into `host-[a:b]-c` and `22`
-func getHostPort(str string) (string, int, error) {
-	port := 22
-	parts := strings.Split(str, ":")
-	if len(parts) == 1 {
-		return str, port, nil
-	}
-	lastPart := parts[len(parts)-1]
-	if strings.Contains(lastPart, "]") {
-		// We are in expand pattern, so no port were specified
-		return str, port, nil
-	}
-	port, err := strconv.Atoi(lastPart)
-	return strings.Join(parts[:len(parts)-1], ":"), port, err
+func (group *Group) String() string {
+	return group.Name
 }
 
-// expandHostPattern turns `host-[a:b]-c` into a flat list of hosts
-func expandHostPattern(hostpattern string) ([]string, error) {
-	lbrac := strings.Replace(hostpattern, "[", "|", 1)
-	rbrac := strings.Replace(lbrac, "]", "|", 1)
-	parts := strings.Split(rbrac, "|")
+func (host *Host) String() string {
+	return host.Name
+}
 
-	if len(parts) == 1 {
-		// No pattern detected
-		return []string{hostpattern}, nil
-	}
-	if len(parts) != 3 {
-		return nil, fmt.Errorf("wrong host pattern: %s", hostpattern)
-	}
-
-	head, nrange, tail := parts[0], parts[1], parts[2]
-	bounds := strings.Split(nrange, ":")
-	if len(bounds) < 2 || len(bounds) > 3 {
-		return nil, fmt.Errorf("wrong host pattern: %s", hostpattern)
-	}
-
-	var begin, end []rune
-	var step = 1
-	if len(bounds) == 3 {
-		step, _ = strconv.Atoi(bounds[2])
-	}
-
-	end = []rune(bounds[1])
-	if bounds[0] == "" {
-		if isRunesNumber(end) {
-			format := fmt.Sprintf("%%0%dd", len(end))
-			begin = []rune(fmt.Sprintf(format, 0))
-		} else {
-			return nil, fmt.Errorf("skipping range start in not allowed with alphabetical range: %s", hostpattern)
+func groupMapToLower(groups map[string]*Group, keysOnly bool) map[string]*Group {
+	newGroups := make(map[string]*Group, len(groups))
+	for groupname, group := range groups {
+		groupname = strings.ToLower(groupname)
+		if !keysOnly {
+			group.Name = groupname
+			group.DirectParents = groupMapToLower(group.DirectParents, true)
+			group.Parents = groupMapToLower(group.Parents, true)
+			group.Children = groupMapToLower(group.Children, true)
 		}
-	} else {
-		begin = []rune(bounds[0])
+		newGroups[groupname] = group
 	}
-
-	var chars []int
-	isNumberRange := false
-
-	if isRunesNumber(begin) && isRunesNumber(end) {
-		chars = makeRange(runesToInt(begin), runesToInt(end), step)
-		isNumberRange = true
-	} else if !isRunesNumber(begin) && !isRunesNumber(end) && len(begin) == 1 && len(end) == 1 {
-		dict := append(makeRange('a', 'z', 1), makeRange('A', 'Z', 1)...)
-		chars = makeRange(
-			find(dict, int(begin[0])),
-			find(dict, int(end[0])),
-			step,
-		)
-		for i, c := range chars {
-			chars[i] = dict[c]
-		}
-	}
-
-	if len(chars) == 0 {
-		return nil, fmt.Errorf("bad range specified: %s", nrange)
-	}
-
-	var hosts []string
-	var format string
-	if isNumberRange {
-		format = fmt.Sprintf("%%s%%0%dd%%s", len(begin))
-	} else {
-		format = "%s%c%s"
-	}
-
-	for _, c := range chars {
-		hosts = append(hosts, fmt.Sprintf(format, head, c, tail))
-	}
-
-	var result []string
-	for _, hostpattern := range hosts {
-		newHosts, err := expandHostPattern(hostpattern)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, newHosts...)
-	}
-
-	return result, nil
-}
-
-func isRunesNumber(runes []rune) bool {
-	for _, rune := range runes {
-		if rune < '0' || rune > '9' {
-			return false
-		}
-	}
-	return true
-}
-
-// runesToInt turn runes into corresponding number, ex. '7' -> 7
-// should be called only on "number" runes! (see `isRunesNumber` function)
-func runesToInt(runes []rune) int {
-	result := 0
-	for i, rune := range runes {
-		result += int((rune - '0')) * int(math.Pow10(len(runes)-1-i))
-	}
-	return result
-}
-
-func makeRange(start, end, step int) []int {
-	s := make([]int, 0, 1+(end-start)/step)
-	for start <= end {
-		s = append(s, start)
-		start += step
-	}
-	return s
-}
-
-func find(a []int, x int) int {
-	for i, n := range a {
-		if x == n {
-			return i
-		}
-	}
-	return len(a)
+	return newGroups
 }
